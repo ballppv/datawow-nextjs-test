1d17b08af4192ad1c27a836453f278b977360048
Ball Pornpavee <pornpavee.ji@gmail.com>
2024-06-29 03:59:10 +0700

    setting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/layouts/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/layouts/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        sm: '320px',
+        mobile: '375px',
+        md: '768px',
+        '2md': '900px',
+        lg: '1024px',
+        xl: '1440px',
       },
+      fontSize: {
+        '3xs': ['8px', '12px'],
+      },
+      colors: {},
+      backgroundColor: {},
+      borderColor: {},
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
